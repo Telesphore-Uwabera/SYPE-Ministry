67750f553467783c63c8a069ee139394c9f76766
Telesphore Uwabera <t.uwabera@alustudent.com>
2026-05-09 20:56:57 +0200

    :

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -808,7 +808,7 @@ export default function Home() {
           </motion.h2>
 
           <motion.div
-            className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8"
+            className="grid grid-cols-2 lg:grid-cols-4 gap-8"
             variants={containerVariants}
             initial="hidden"
             whileInView="visible"
